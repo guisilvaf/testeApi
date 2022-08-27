abb5aecaaa17d6842325a4d1cbf2a974ec47b1d6
guisilvaf <guilsilvaf@gmail.com>
2022-08-27 16:13:44 -0300

    Price correction.

--- testeApi/main.py
+++ testeApi/main.py
@@ -1,17 +1,22 @@
 import requests
 import json
+import datetime
 
 
-# api = requests.get("")  #Request para pegar a api da loja
+# api = requests.get("Zj7P7qnkl-D9fRIpMyjFB5iLaK0qVYFTiYEHk1Vj4j8")  #Request para pegar a api da loja.
 
-data = open("items.csv", 'r', encoding='utf-8').readlines()
-dictionary = dict.fromkeys(data)
+data = open("items.csv", 'r', encoding='utf-8').readlines() #Abrindo planilha.
+dictionary = dict.fromkeys(data) #Transformando a planilha em um dictionary
 
+#Abrindo a planilha como .JSON.
 with open(r'data.json', 'w', encoding='utf-8') as file:
     json.dump(dictionary, file, indent=2)
 
+def floatPrice():
+    for price in dictionary['item']:
+        round(['Preco_regular'], 2) #Arredondando o preço para 2 casas decimais
+        float(['Preco_regular']) #Transformando o preço em float
 
-#print(data)
-print(type(dictionary))
 
-# requests.post("", data={}) #Request para enviar informações para a API
+'''requests.put("Zj7P7qnkl-D9fRIpMyjFB5iLaK0qVYFTiYEHk1Vj4j8", dictionary={'internal_code':'Codigo', 'name':'Nome', 'price':'Preco_regular', 'visible':'Ativo',
+ 'stock':'Estoque', 'barcodes':'Codigo_barras', 'promo_price':'Promocao', 'promo_end_at':'Termino_promocao'}) #Request para enviar informações para a API. '''
